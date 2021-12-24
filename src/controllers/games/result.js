1d685f839b78d4b3754e004f6c4b8b29b51694cc
lopezrunco <lopezrunco@gmail.com>
2021-12-24 13:50:16 -0300

    Solucion error de ids acumulados en winnersId

--- src/controllers/games/result.js
+++ src/controllers/games/result.js
@@ -1,29 +1,30 @@
 const { gameModel } = require('../../models/game')
 
 module.exports = (request, response) => {
-
     gameModel
         .findOne({ _id: request.params.id })
         .then(game => {
-            // Definir ganador
+
+            // Limpia el array de ganadores para que no se acumulen los push con cada peticion
+            game.movesWinners = []
+
+            // Define el ganador en cada jugada y empuja el id del ganador al array de ganadores
             for (let i = 0; i < 3; i++) {
                 if (game.playerOneMoves[i].losesTo === game.playerTwoMoves[i].choice) {
-                    console.log('Usuario one pierde --------------------------------------')
                     game.movesWinners.push(game.playerTwoId)
                 } else if (game.playerTwoMoves[i].losesTo === game.playerOneMoves[i].choice) {
-                    console.log('Usuario one gana ----------------------------------------')
                     game.movesWinners.push(game.playerOneId)
                 } else if (game.playerOneMoves[i].choice === game.playerOneMoves[i].choice) {
-                    console.log('Empate ----------------------------------------')
+                    console.log('Manejar empate ----------------------------------------')
                 }
             }
-
             game.save()
 
-        }).then(() => {
             response.status(200).json({
-                message: 'Estos son los resultados'
+                message: 'Game results:',
+                game
             })
+            
         }).catch(error => {
             console.error(error)
             response.status(500).json({
