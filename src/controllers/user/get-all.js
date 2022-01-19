756d17c7a8e6583a43c0579d0ad672726f4bdfc7
lopezrunco <lopezrunco@gmail.com>
2022-01-19 15:48:53 -0300

    Logica base get-opponents

--- src/controllers/user/get-all.js
+++ src/controllers/user/get-all.js
@@ -1,46 +1,80 @@
-const { userModel } = require("../../models/user");
+const { userModel } = require("../../models/user")
+const { gameModel } = require('../../models/game')
 
 module.exports = (request, response) => {
-    const pagination = {
-        offset: 0,
-        limit: 12
-    }
-    if (request.query.page && request.query.itemsPerPage) {
-        pagination.offset = (request.query.page - 1) * request.query.itemsPerPage,
-            pagination.limit = parseInt(request.query.itemsPerPage)
-    }
+    // const pagination = {
+    //     offset: 0,
+    //     limit: 12
+    // }
+    // if (request.query.page && request.query.itemsPerPage) {
+    //     pagination.offset = (request.query.page - 1) * request.query.itemsPerPage,
+    //         pagination.limit = parseInt(request.query.itemsPerPage)
+    // }
 
-    userModel
+    gameModel
         .find({
             $and: [
-                { _id: {$ne: request.user.id} } // Obtiene todos los usuarios menos el que esta logueado
+                {
+                    $or: [
+                        { playerOneId: request.user.id },
+                        { playerTwoId: request.user.id }
+                    ]
+                }, { completed: false }
             ]
         })
-        .select('-email -password -mfaEnabled -mfaSecret')  // Omision de campos
-        .skip(pagination.offset)
-        .limit(pagination.limit)
-        .then(users => {
+        .select('playerOneId playerTwoId')
+        .then(games => {
+
+            let idsObtained = []
+            games.map((game, index) => {
+                idsObtained.push(games[index].playerOneId)
+                idsObtained.push(games[index].playerTwoId)
+            })
+
+
             userModel
-                .count()
-                .then(count => {
-                    const meta = {
-                        count
-                    }
-                    // Responde con los usuarios y el numero de usuarios
-                    response.status(200).json({
-                        meta,
+                // .find({ _id: { $nin: [games.playerOneId, games.playerTwoId, '61e7f3a359b3467a62e217b2'] } })
+                .find({ _id: { $nin: idsObtained } })
+                .select('nickname')
+                .then(users => {
+                    response.status(20git branch
+                        0).json({
                         users
                     })
-                }).catch(error => {
-                    console.error(error)
-                    response.status(500).json({
-                        message: 'Error trying to list the users'
-                    })
                 })
-        }).catch(error => {
-            console.error(error)
-            response.status(500).json({
-                message: 'Error trying to list the users'
-            })
         })
+
+    // userModel
+    //     .find({
+    //         $and: [
+    //             { _id: {$ne: request.user.id} } // Obtiene todos los usuarios menos el que esta logueado
+    //         ]
+    //     })
+    //     .select('-email -password -mfaEnabled -mfaSecret')  // Omision de campos
+    //     .skip(pagination.offset)
+    //     .limit(pagination.limit)
+    //     .then(users => {
+    //         userModel
+    //             .count()
+    //             .then(count => {
+    //                 const meta = {
+    //                     count
+    //                 }
+    //                 // Responde con los usuarios y el numero de usuarios
+    //                 response.status(200).json({
+    //                     meta,
+    //                     users
+    //                 })
+    //             }).catch(error => {
+    //                 console.error(error)
+    //                 response.status(500).json({
+    //                     message: 'Error trying to list the users'
+    //                 })
+    //             })
+    //     }).catch(error => {
+    //         console.error(error)
+    //         response.status(500).json({
+    //             message: 'Error trying to list the users'
+    //         })
+    //     })
 }
